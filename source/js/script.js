afbf38163bd44162aaaa39f6031a34ebd5353934
proninserge <60112381+proninserge@users.noreply.github.com>
2020-05-04 17:08:36 +0500

    внесение правок

--- source/js/script.js
+++ source/js/script.js
@@ -1,19 +1,10 @@
 var menu = document.querySelector(".page-nav");
 var openMenu = document.querySelector(".page-header__toggle");
 
+menu.classList.remove("page-nav--no-js");
+openMenu.classList.remove("page-header__toggle--no-js");
+
 openMenu.addEventListener("click", function () {
   menu.classList.toggle("page-nav--closed");
   openMenu.classList.toggle("page-header__toggle--close");
 });
-function widthDepended(notOnMobile) {
-  if (notOnMobile.matches) {
-    menu.classList.add("page-nav--no-js");
-    openMenu.classList.add("page-header__toggle--no-js");
-  } else {
-    menu.classList.remove("page-nav--no-js");
-    openMenu.classList.remove("page-header__toggle--no-js");
-  }
-}
-var notOnMobile = window.matchMedia("(min-width: 768px)");
-notOnMobile.addListener(widthDepended);
-widthDepended(notOnMobile);
--- source/js/submit.js
+++ source/js/submit.js
@@ -1,55 +1,37 @@
-var mainName = document.querySelector("[name=first-name]");
-var lastName = document.querySelector("[name=last-name]");
-var telephone = document.querySelector("[name=phone]");
-var email = document.querySelector("[name=e-mail]");
+var elements = document.querySelectorAll(".feedback-form__type-input--hold");
 var sendForm = document.querySelector(".feedback-form__btn");
 var popupFailed = document.querySelector(".popup--failed");
 var popupDone = document.querySelector(".popup--success");
-var buttonFailure = document.querySelectorAll(".popup__btn--failure");
-var buttonSuccess = document.querySelectorAll(".popup__btn--success");
+var popup = document.querySelectorAll(".popup");
+var buttons = document.querySelectorAll(".popup__btn");
 
-sendForm.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  if (!mainName.value) {
-    mainName.classList.add("feedback-form__type-input--error");
-  }
-  if (!lastName.value) {
-    lastName.classList.add("feedback-form__type-input--error");
-  }
-  if (!telephone.value) {
-    telephone.classList.add("feedback-form__type-input--error");
-  }
-  if (!email.value || !email.checkValidity()) {
-    email.classList.add("feedback-form__type-input--error");
-  }
-  if (!mainName.value || !lastName.value || !telephone.value || !email.value || !email.checkValidity()) {
-    popupFailed.classList.add("popup--show");
-  } else {
-    popupDone.classList.add("popup--show");
+sendForm.addEventListener("click", function (event) {
+  event.preventDefault();
+  var count = 0;
+  for (var i = 0; i <= elements.length - 1; i++) {
+    if (!elements[i].value || !elements[i].checkValidity()) {
+      elements[i].classList.add("feedback-form__type-input--error");
+      popupFailed.classList.add("popup--show");
+    } else {
+      elements[i].classList.remove("feedback-form__type-input--error");
+      count++;
+    }
+    if (count === elements.length) {
+      popupDone.classList.add("popup--show");
+    }
   }
 });
 
-buttonFailure.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  popupFailed.classList.remove("popup--show");
-  mainName.classList.remove("feedback-form__type-input--error");
-  lastName.classList.remove("feedback-form__type-input--error");
-  telephone.classList.remove("feedback-form__type-input--error");
-  email.classList.remove("feedback-form__type-input--error");
-});
-
-buttonSuccess.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  popupDone.classList.remove("popup--show");
-});
+buttons.forEach(function(button) {
+  button.addEventListener("click", function(evt) {
+    evt.target.closest(".popup").classList.remove("popup--show");
+  });
+})
 
 window.addEventListener("keydown", function (evt) {
   if (evt.keyCode === 27) {
-    if (popupFailed.classList.contains("popup--show")) {
-      popupFailed.classList.remove("popup--show");
-    }
-    if (popupDone.classList.contains("popup--show")) {
-      popupDone.classList.remove("popup--show");
+    for (var i = 0; i <= popup.length - 1; i++) {
+      popup[i].classList.remove("popup--show");
     }
   }
 });
